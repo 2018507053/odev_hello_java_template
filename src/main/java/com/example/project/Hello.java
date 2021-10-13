82f9a8ca6ddee412850107767deadeefa885a2a8
Alaettin UÇAN <ucanalaettin@gmail.com>
2021-10-14 00:06:06 +0300

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -3,7 +3,7 @@ package com.example.project;
 public class Hello {
 
 	public static void main(final String[] args) {
-		//bu fonksiyonun çıktısını "Hello OKUYBS!" yapınız!
+		//bu fonksiyonun ciktisini "Hello OKUYBS!" yapiniz!
 		System.out.println("Not the right string, tests will fail!");
 	}
 
